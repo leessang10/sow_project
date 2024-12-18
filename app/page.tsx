bdc95b75e5d7a98a9d474a02b16fd97f54b4f226
sangmu.lee <leessang10@me.com>
2024-12-19 00:33:25 +0900

    feat: 메인 페이지 > 최근 프로젝트 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,6 @@
 
 import {motion} from "framer-motion";
 
-const containerVariants = {
-    hidden: {opacity: 0},
-    visible: {
-        opacity: 1,
-        transition: {
-            staggerChildren: 0.2
-        }
-    }
-};
-const itemVariants = {
-    hidden: {opacity: 0, y: 20},
-    visible: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 0.5
-        }
-    }
-};
 export default function Home() {
     return (
         <main className="min-h-screen bg-white dark:bg-dark-bg transition-colors">
